7e6897b136bddbc0529742913715cca40fba2bb7
Siyam <acetsiyam@gmail.com>
2024-07-21 20:39:39 +0300

    commend lines added to main.go

--- main.go
+++ main.go
@@ -8,6 +8,11 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
+// MainPage is a handler function for the main page of the website.
+// It takes in the http.ResponseWriter, http.Request, and httprouter.Params as parameters.
+// It parses the "index.html" template file and executes it with the provided data.
+// The data is retrieved from the "slug" parameter in the URL.
+// Finally, it prints "MainPage" to the console.
 func MainPage(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 
 	view, _ := template.ParseFiles("index.html")
